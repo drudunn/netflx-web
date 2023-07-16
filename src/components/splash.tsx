92a2430e99934f69cafc2cc5e7a22f036a7b8027
Andru Dunn <andru@dunn.wtf>
2023-07-16 19:41:46 +0100

    fix: handleClick tslint

--- src/components/splash.tsx
+++ src/components/splash.tsx
@@ -9,11 +9,18 @@ export const Splash = () => {
     autoPlay: false,
   });
 
+  const handleClick = () => {
+    if (!state.playing) {
+      controls.play()?.catch(err => console.warn('issue playing', err))
+    }
+    return
+  }
+
   return (
     <main className="flex h-screen">
       <div className="m-auto">
         <div className="sr-only">{audio}</div>
-        <motion.img src="/dunnflix.svg" onClick={controls.play} whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} />
+        <motion.img src="/dunnflix.svg" onClick={handleClick} whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} />
         <h1 className="text-center"><span className="sr-only">Dunnflix </span>coming soon</h1>
       </div>
     </main>
