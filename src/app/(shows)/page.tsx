dcd12830ee98c18172200cf4ddc2e784debad36b
Andru Dunn <andru@dunn.wtf>
2023-11-13 00:57:23 +0000

    refactor: homepage shows

--- src/app/(shows)/page.tsx
+++ src/app/(shows)/page.tsx
@@ -1,51 +1,56 @@
-import type { CategorizedShows } from "@/types"
+import type { CategorizedShows, Show } from '@/types'
 
-import { getShows } from "@/lib/fetchers"
-import { getCurrentUser } from "@/lib/session"
-import Hero from "@/components/hero"
-import ShowsContainer from "@/components/shows-container"
+import { getShows } from '@/lib/fetchers'
+import { getCurrentUser } from '@/lib/session'
+import Hero from '@/components/hero'
+import ShowsContainer from '@/components/shows-container'
+import { ourShows } from '@/app/(shows)/shows';
 
 export default async function Home() {
   const user = await getCurrentUser()
 
-  const allShows = await getShows("movie")
+  const shows = ourShows as unknown as Show[]
+  const favorites = shows.slice(0, 6)
+  const topPicks = shows.slice(6)
+
+  const allShows = await getShows('movie')
 
   const allShowsByCategory: CategorizedShows[] = [
     {
-      title: "Trending Now",
-      shows: allShows.trending,
+      title: `Andru & Christina's Favorites`,
+      shows: favorites,
     },
     {
-      title: "Top Rated",
-      shows: allShows.topRated,
+      title: `Andru & Christina's Top Picks`,
+      shows: topPicks,
     },
     {
-      title: "Action Thrillers",
-      shows: allShows.action,
-    },
-    {
-      title: "Comedies",
-      shows: allShows.comedy,
-    },
-    {
-      title: "Scary Movies",
-      shows: allShows.horror,
-    },
-    {
-      title: "Romance Movies",
-      shows: allShows.romance,
-    },
-    {
-      title: "Documentaries",
+      title: 'Travel Highlights',
       shows: allShows.docs,
     },
+    {
+      title: "The Ultimate Best Man",
+      shows: allShows.comedy,
+    },
+    // {
+    //   title: "Scary Movies",
+    //   shows: allShows.horror,
+    // },
+    // {
+    //   title: "Romance Movies",
+    //   shows: allShows.romance,
+    // },
+    // {
+    //   title: "Documentaries",
+    //   shows: allShows.docs,
+    // },
   ]
 
   return (
     <section>
       <div className="pb-16 pt-10">
-        <Hero shows={allShows.netflix ?? []} />
-        <ShowsContainer user={user} shows={allShowsByCategory} />
+        <Hero shows={shows ?? []}/>
+        <ShowsContainer user={user} shows={allShowsByCategory}/>
       </div>
     </section>
   )
---
+++ src/app/(shows)/shows.ts
@@ -0,0 +1,284 @@
+
+const brooklyn99 = {
+  "adult": false,
+  "backdrop_path": "/9AeiA1XtP5sel2tAf9LaGeUjhDb.jpg",
+  "id": 48891,
+  "name": "Brooklyn Nine-Nine",
+  "original_language": "en",
+  "original_name": "Brooklyn Nine-Nine",
+  "overview": "A single-camera ensemble comedy following the lives of an eclectic group of detectives in a New York precinct, including one slacker who is forced to shape up when he gets a new boss.",
+  "poster_path": "/hgRMSOt7a1b8qyQR68vUixJPang.jpg",
+  "media_type": "tv",
+  "genre_ids": [
+    35,
+    80
+  ],
+  "popularity": 436.476,
+  "first_air_date": "2013-09-17",
+  "vote_average": 8.227,
+  "vote_count": 2929,
+  "origin_country": [
+    "US"
+  ]
+}
+
+const theOffice = {
+  "adult": false,
+  "backdrop_path": "/slVhck5n2GwlgPFWKJGfqiYyzCd.jpg",
+  "id": 2316,
+  "name": "The Office",
+  "original_language": "en",
+  "original_name": "The Office",
+  "overview": "The everyday lives of office employees in the Scranton, Pennsylvania branch of the fictional Dunder Mifflin Paper Company.",
+  "poster_path": "/7DJKHzAi83BmQrWLrYYOqcoKfhR.jpg",
+  "media_type": "tv",
+  "genre_ids": [
+    35
+  ],
+  "popularity": 391.664,
+  "first_air_date": "2005-03-24",
+  "vote_average": 8.589,
+  "vote_count": 3510,
+  "origin_country": [
+    "US"
+  ]
+}
+
+const parksAndRec = {
+  "adult": false,
+  "backdrop_path": "/v7FPlZuuwJZ6YwJggf3Jx5pZNAz.jpg",
+  "id": 8592,
+  "name": "Parks and Recreation",
+  "original_language": "en",
+  "original_name": "Parks and Recreation",
+  "overview": "Hilarious ensemble comedy that follows Leslie Knope, a mid-level bureaucrat in the Parks and Recreation Department of Pawnee, Indiana, and her tireless efforts to make her quintessentially American town just a little bit more fun.",
+  "poster_path": "/dDuzrl9rUIBYieZjqmtNCdncrmZ.jpg",
+  "media_type": "tv",
+  "genre_ids": [
+    35
+  ],
+  "popularity": 265.428,
+  "first_air_date": "2009-04-09",
+  "vote_average": 8.03,
+  "vote_count": 1281,
+  "origin_country": [
+    "US"
+  ]
+}
+
+const tedLasso = {
+  "adult": false,
+  "backdrop_path": "/wImNeqxKsqmJ5OBw8j3I37GNFN3.jpg",
+  "id": 97546,
+  "name": "Ted Lasso",
+  "original_language": "en",
+  "original_name": "Ted Lasso",
+  "overview": "Ted Lasso, an American football coach, moves to England when he’s hired to manage a soccer team—despite having no experience. With cynical players and a doubtful town, will he get them to see the Ted Lasso Way?",
+  "poster_path": "/5fhZdwP1DVJ0FyVH6vrFdHwpXIn.jpg",
+  "media_type": "tv",
+  "genre_ids": [
+    35,
+    18
+  ],
+  "popularity": 160.823,
+  "first_air_date": "2020-08-14",
+  "vote_average": 8.466,
+  "vote_count": 1313,
+  "origin_country": [
+    "GB"
+  ]
+}
+
+const jurasicPark = {
+  "adult": false,
+  "backdrop_path": "/79bJL9ydAMYVltuNTt4VhxORqIz.jpg",
+  "id": 329,
+  "title": "Jurassic Park",
+  "original_language": "en",
+  "original_title": "Jurassic Park",
+  "overview": "A wealthy entrepreneur secretly creates a theme park featuring living dinosaurs drawn from prehistoric DNA. Before opening day, he invites a team of experts and his two eager grandchildren to experience the park and help calm anxious investors. However, the park is anything but amusing as the security systems go off-line and the dinosaurs escape.",
+  "poster_path": "/oU7Oq2kFAAlGqbU4VoAE36g4hoI.jpg",
+  "media_type": "movie",
+  "genre_ids": [
+    12,
+    878
+  ],
+  "popularity": 22.711,
+  "release_date": "1993-06-11",
+  "video": false,
+  "vote_average": 7.943,
+  "vote_count": 15265
+}
+
+const onlyMurders = {
+  "adult": false,
+  "backdrop_path": "/sMPGLb2cjsE7K4T0h4Vsiv93V5J.jpg",
+  "id": 107113,
+  "name": "Only Murders in the Building",
+  "original_language": "en",
+  "original_name": "Only Murders in the Building",
+  "overview": "Three strangers share an obsession with true crime and suddenly find themselves wrapped up in one.",
+  "poster_path": "/yhx6PnU3L2a6FnEFGOlBKTZ8TSD.jpg",
+  "media_type": "tv",
+  "genre_ids": [
+    35,
+    9648,
+    80
+  ],
+  "popularity": 106.219,
+  "first_air_date": "2021-08-31",
+  "vote_average": 8.424,
+  "vote_count": 1158,
+  "origin_country": [
+    "US"
+  ]
+}
+
+const schittsCreek = {
+  "adult": false,
+  "backdrop_path": "/ftNmVV8LuNVmtB8mzoUTucozVKE.jpg",
+  "id": 61662,
+  "name": "Schitt's Creek",
+  "original_language": "en",
+  "original_name": "Schitt's Creek",
+  "overview": "Formerly filthy rich video store magnate Johnny Rose, his soap star wife Moira, and their two kids, über-hipster son David and socialite daughter Alexis, suddenly find themselves broke and forced to live in Schitt's Creek, a small depressing town they once bought as a joke.",
+  "poster_path": "/iRfSzrPS5VYWQv7KVSEg2BZZL6C.jpg",
+  "media_type": "tv",
+  "genre_ids": [
+    35
+  ],
+  "popularity": 120.067,
+  "first_air_date": "2015-01-13",
+  "vote_average": 7.777,
+  "vote_count": 412,
+  "origin_country": [
+    "CA"
+  ]
+}
+
+const theBear = {
+  "adult": false,
+  "backdrop_path": "/wHNwlE6ftEpgjVbdhLXOtv1hLs0.jpg",
+  "id": 136315,
+  "name": "The Bear",
+  "original_language": "en",
+  "original_name": "The Bear",
+  "overview": "Carmy, a young fine-dining chef, comes home to Chicago to run his family sandwich shop. As he fights to transform the shop and himself, he works alongside a rough-around-the-edges crew that ultimately reveal themselves as his chosen family.",
+  "poster_path": "/3MVHF64rlvH1eofKefIoazXwOK0.jpg",
+  "media_type": "tv",
+  "genre_ids": [
+    18,
+    35
+  ],
+  "popularity": 120.1,
+  "first_air_date": "2022-06-23",
+  "vote_average": 8.256,
+  "vote_count": 627,
+  "origin_country": [
+    "US"
+  ]
+}
+
+const trying = {
+  "adult": false,
+  "backdrop_path": "/jeGWGcS2OrCvwFh3NBb3KxX7OZY.jpg",
+  "id": 98177,
+  "name": "Trying",
+  "original_language": "en",
+  "original_name": "Trying",
+  "overview": "All Nikki and Jason want is a baby—the one thing they can’t have. So they decide to adopt. With their dysfunctional friends, screwball families, and chaotic lives, will the adoption panel agree that they’re ready to be parents?",
+  "poster_path": "/cRMpyWmKinhFQe83iTIk828INuE.jpg",
+  "media_type": "tv",
+  "genre_ids": [
+    35
+  ],
+  "popularity": 21.988,
+  "first_air_date": "2020-05-01",
+  "vote_average": 7.859,
+  "vote_count": 64,
+  "origin_country": [
+    "GB"
+  ]
+}
+
+const alwaysSunny = {
+  "adult": false,
+  "backdrop_path": "/i7TBu5x8eCSD4Os2UYUuUVU8vEO.jpg",
+  "id": 2710,
+  "name": "It's Always Sunny in Philadelphia",
+  "original_language": "en",
+  "original_name": "It's Always Sunny in Philadelphia",
+  "overview": "Four egocentric friends run a neighborhood Irish pub in Philadelphia and try to find their way through the adult world of work and relationships. Unfortunately, their warped views and precarious judgments often lead them to trouble, creating a myriad of uncomfortable situations that usually only get worse before they get better.",
+  "poster_path": "/pRWO6ufqSNkWvPXDDQhBwPNSv4K.jpg",
+  "media_type": "tv",
+  "genre_ids": [
+    35
+  ],
+  "popularity": 303.23,
+  "first_air_date": "2005-08-04",
+  "vote_average": 8.29,
+  "vote_count": 946,
+  "origin_country": [
+    "US"
+  ]
+}
+
+const mando = {
+  "adult": false,
+  "backdrop_path": "/9zcbqSxdsRMZWHYtyCd1nXPr2xq.jpg",
+  "id": 82856,
+  "name": "The Mandalorian",
+  "original_language": "en",
+  "original_name": "The Mandalorian",
+  "overview": "After the fall of the Galactic Empire, lawlessness has spread throughout the galaxy. A lone gunfighter makes his way through the outer reaches, earning his keep as a bounty hunter.",
+  "poster_path": "/eU1i6eHXlzMOlEq0ku1Rzq7Y4wA.jpg",
+  "media_type": "tv",
+  "genre_ids": [
+    10765,
+    10759,
+    18
+  ],
+  "popularity": 198.005,
+  "first_air_date": "2019-11-12",
+  "vote_average": 8.463,
+  "vote_count": 9437,
+  "origin_country": [
+    "US"
+  ]
+}
+
+const nemo = {
+  "adult": false,
+  "backdrop_path": "/h3uqFk7sZRJvLZDdLiFB9qwbL07.jpg",
+  "id": 12,
+  "title": "Finding Nemo",
+  "original_language": "en",
+  "original_title": "Finding Nemo",
+  "overview": "Nemo, an adventurous young clownfish, is unexpectedly taken from his Great Barrier Reef home to a dentist's office aquarium. It's up to his worrisome father Marlin and a friendly but forgetful fish Dory to bring Nemo home -- meeting vegetarian sharks, surfer dude turtles, hypnotic jellyfish, hungry seagulls, and more along the way.",
+  "poster_path": "/ggQ6o8X5984OCh3kZi2UIJQJY5y.jpg",
+  "media_type": "movie",
+  "genre_ids": [
+    16,
+    10751
+  ],
+  "popularity": 94.569,
+  "release_date": "2003-05-30",
+  "video": false,
+  "vote_average": 7.824,
+  "vote_count": 18185
+}
+
+export const ourShows = [
+  schittsCreek,
+  theOffice,
+  brooklyn99,
+  theBear,
+  parksAndRec,
+  tedLasso,
+  jurasicPark,
+  onlyMurders,
+  trying,
+  alwaysSunny,
+  mando,
+  nemo
+]
